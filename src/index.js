0763664cdc88b024a58a9b22846822a8a234c57d
Alim Bolar <alim@fourplusmedia.com>
2022-03-03 10:26:57 +0800

    added js functionality to nav

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./style.scss";
+
+const sections = document.querySelectorAll("main>section");
+console.log(sections);
+const nav = document.querySelector(".nav");
+
+console.log(nav);
+
+nav.addEventListener("click", displayView);
+
+function displayView(e) {
+  e.preventDefault();
+
+  const targetlink = e.target.closest(".nav__item");
+
+  if (!targetlink) return;
+  const clickedLink = targetlink.dataset.link;
+
+  const targetSection = document.querySelector(`.${clickedLink}`);
+
+  sections.forEach((section) => section.classList.add("hidden"));
+
+  console.log(targetSection);
+
+  targetSection.classList.remove("hidden");
+}
